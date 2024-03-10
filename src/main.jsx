c7fe7d96b563ec6849764bec7f86a4e1089476e4
arad55500 <arad55501@gmail.com>
2024-03-10 15:53:40 -0400

    comment

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react' // quicker-photo-tuneup/src/main.jsx
+import ReactDOM from 'react-dom/client' // quicker-photo-tuneup/src/index.jsx
 import App from './App.jsx' // quicker-photo-tuneup/src/App.jsx
-import './index.css'
+import './index.css' // quicker-photo-tuneup/src/index.css
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
